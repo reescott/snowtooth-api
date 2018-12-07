178e6db4b77739459e2838d89cb98a4f5ee65061
Scott Rees <srees@worldvision.org>
2018-12-06 16:02:19 -0800

    Day 1 changes - adding Trail and Lift objects

--- index.js
+++ index.js
@@ -1,4 +1,83 @@
+const { ApolloServer, PubSub } = require('apollo-server')
+
 const lifts = require('./data/lifts.json')
 const trails = require('./data/trails.json')
 
 console.log(`Build your GraphQL Server Here!`)
+/**
+ * "capacity": 1,
+    "status": "OPEN",
+    "night": false,
+    "elevationGain": 50,
+    "trails": ["home-run", "crosscut"],
+ */
+
+const typeDefs = `
+  enum Status {
+    OPEN
+    CLOSED
+    HOLD
+  }
+  type Lift {
+    id: ID!
+    name:String!
+    capacity: Int!
+    status: Status!
+    night: Boolean
+    elevationGain: Int
+    trailAccess: [Trail!]!
+  }
+  type Trail {
+    id: ID
+    name: String
+    accessedByLifts: [Lift!]!
+    status: Status!
+    groomed: Boolean
+    snowmaking: Boolean
+    trees: Boolean
+    night: Boolean
+  }
+  type Query {
+    allLifts: [Lift!]!
+  }
+  type Mutation {
+    setLiftStatus(id: ID!, status: Status!): Lift!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    allLifts: () => {
+      return lifts
+    }
+  },
+  Mutation: {
+    setLiftStatus: (parent, args) => {
+      const liftId = args.id;
+      const liftStatus = args.status;
+      let updatedLift = lifts.find(lift => liftId === lift.id);
+      updatedLift.status = liftStatus;
+      return updatedLift;
+    }
+  },
+  Lift: {
+    trailAccess: (parent) => {
+      return parent.trails.map(id => trails.find(t=> id === t.id)).filter(x=>x)
+    }
+  },
+  Trail: {
+    accessedByLifts: (parent) => {
+      return parent.lift.map(id => lifts.find(l => id === l.id)).filter(x => x)
+    }
+  }
+
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.listen().then(({ url })=> {
+  console.log(`Server running at ${url}`);
+});
